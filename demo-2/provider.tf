55a7b7fec3486fd00a4460511dc019620e5a5975
Volodymyr Kryzhanivskyi <mail@vov4ik.pp.ua>
2019-02-18 21:00:55 +0200

    updated second demo

--- demo-2/provider.tf
+++ demo-2/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-    access_key = "${var.AWS_ACCESS_KEY}"
-    secret_key = "${var.AWS_SECRET_KEY}"
-    region = "${var.AWS_REGION}"
+  region                  = "${var.AWS_REGION}"
+  shared_credentials_file = "${var.HOME_PATH}/.aws/credentials"
+  profile                 = "default"
 }
-
--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -1,23 +1,23 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "HOME_PATH" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-2"
 }
+
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-2 = "ami-0e7589a8422e3270f"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-1 = "ami-0d729a60"
   }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
 }
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+
+variable "KEY_NAME" {
 }
+
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
 }
