a29d0868ac000ebad51ca599bf4305f3890cad4a
Volodymyr Kryzhanivskyi <mail@vov4ik.pp.ua>
2019-02-18 20:23:42 +0200

    updated first demo

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,12 +1,12 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "HOME_PATH" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-2"
 }
+
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-2 = "ami-0e7589a8422e3270f"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-0d729a60"
   }
